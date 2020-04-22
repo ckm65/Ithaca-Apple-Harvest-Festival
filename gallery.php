9853661ee290d8474010754f70dd8abbed205b38
Carolyn Morikawa <ckm65@cornell.edu>
2020-04-22 14:03:58 -0400

    zoom requirest m3m2

--- gallery.php
+++ gallery.php
@@ -3,6 +3,40 @@ include("includes/init.php");
 $header_nav_class3 = "current_page";
 $title = "Gallery";
 
+// Get a list of animals from the database
+$photography = exec_sql_query($db, "SELECT * FROM photo;")->fetchAll();
+
+// Initialize default values
+$retry_search = FALSE;
+$show_results = FALSE;
+
+// Get search value
+$sticky_search = $_GET['search'];
+
+// Was an request for an animal sent?
+if (empty($sticky_search)) {
+  // No value sent, or value was empty. Don't show the results.
+} else {
+  $search = trim($sticky_search);
+  $search = strtolower($search);
+
+  $sql = "SELECT * FROM photo WHERE (name LIKE '%' || :search || '%')";
+  $params = array(
+    ':search' => $search
+  );
+  $photography= exec_sql_query($db, $sql, $params)->fetchAll();
+}
+
+function photography_element($photography)
+{ ?>
+  <a href="gallery.php?<?php echo http_build_query(array('search' => strtolower($photography['name']))); ?>">
+    <figure>
+      <img src="uploads/festival/<?php echo $photography['id']; ?>.jpg" alt="<?php echo $photography['name']; ?>" />
+      <figcaption><?php echo ucfirst($photography['name']); ?></figcaption>
+    </figure>
+  </a>
+<?php
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,109 +53,33 @@ $header_nav_class = "current_page";
 
 ?>
 
-    <div class="images1">
-        <figure>
-        <!-- Source: https://www.downtownithaca.com/wp-content/uploads/AppleHarvest_Market.jpg -->
-        <img src = "images/gallery1.jpg" alt = "Gallery1" />
-        Source: <cite><a href="https://www.downtownithaca.com/wp-content/uploads/AppleHarvest_Market.jpg">Gallery</a></cite>
-        <figcaption>Gallery</figcaption>
-    </figure>
-    <figure>
-        <!-- Source: https://www.downtownithaca.com/wp-content/uploads/AH_doughtnuts.jpg -->
-        <img src = "images/gallery2.jpg" alt= "Gallery2" />
-        Source: <cite><a href="https://www.downtownithaca.com/wp-content/uploads/AH_doughtnuts.jpg">Gallery</a></cite>
-    <figcaption>Gallery</figcaption>
-    </figure>
-    <figure>
-        <!-- Source:https://d33wubrfki0l68.cloudfront.net/images/festimgs/fc9297fc2b432afb1276125637553bbe429827b8/img_4454.jpg -->
-        <img src = "images/gallery3.jpg
-        " alt = "Gallery3"/>
-        Source: <cite><a href="https://d33wubrfki0l68.cloudfront.net/images/festimgs/fc9297fc2b432afb1276125637553bbe429827b8/img_4454.jpg">Gallery</a></cite>
-        <figcaption>Gallery</figcaption>
-    </figure>
+    <form id="photoForm" action="gallery.php" method="get">
+      <label for="photo_field">Search:</label>
+      <input type="text" id="photo_field" name="search" value="<?php echo htmlspecialchars(!empty($sticky_search) ? $sticky_search : ''); ?>" />
+
+      <button type="submit">Check</button>
+    </form>
+
+    <div class="photo_gallery">
+      <?php
+      if (count($photography) > 0) {
+        foreach ($photography as $p) {
+          photography_element($p);
+        }
+      } else { ?>
+        <p>No photographs matched your search.</p>
+      <?php } ?>
     </div>
 
-    <div class = "images2">
-        <figure>
-        <!-- Source:https://www.eventcrazy.com/event/photos/352755_1_51_091611_115330.jpg -->
-        <img src = "images/gallery4.jpg
-        " alt = "Gallery4"/>
-        Source: <cite><a href="https://www.eventcrazy.com/event/photos/352755_1_51_091611_115330.jpg">Gallery</a></cite>
-        <figcaption>Gallery</figcaption>
-    </figure>
+    <?php if ($sticky_search) { ?>
+      <form action="gallery.php" method="get">
+        <button type="submit">Clear Search</button>
+      </form>
+    <?php } ?>
 
-    <figure>
-        <!-- Source:https://images.localist.com/photos/554201/original/b50047fbde1c35d0c63cdfde07b1d9fcc654d59e.jpg -->
-        <img src = "images/gallery5.jpg
-        " alt = "Gallery5"/>
-        Source: <cite><a href="https://images.localist.com/photos/554201/original/b50047fbde1c35d0c63cdfde07b1d9fcc654d59e.jpg">Gallery</a></cite>
-        <figcaption>Gallery</figcaption>
-    </figure>
+  </main>
 
-    <figure>
-        <!-- Source:https://i2.wp.com/cornellsun.com/wp-content/uploads/2019/09/Pg-1-Apple-Fest-by-Boris-Tsang.jpg?fit=1170%2C781 -->
-        <img src = "images/gallery6.jpg
-        " alt = "Gallery6"/>
-        Source: <cite><a href="https://i2.wp.com/cornellsun.com/wp-content/uploads/2019/09/Pg-1-Apple-Fest-by-Boris-Tsang.jpg?fit=1170%2C781">Gallery</a></cite>
-        <figcaption>Gallery</figcaption>
-    </figure>
-    </div>
-    <div class="images3">
-    <figure>
-        <!-- Source:https://i0.wp.com/ithacavoice.com/wp-content/uploads/2016/09/Screen-Shot-2015-10-02-at-2.19.29-PM.png?resize=620%2C420&ssl=1 -->
-        <img src = "images/gallery7.png
-        " alt = "Gallery7"/>
-        Source: <cite><a href="https://i0.wp.com/ithacavoice.com/wp-content/uploads/2016/09/Screen-Shot-2015-10-02-at-2.19.29-PM.png?resize=620%2C420&ssl=1">Gallery</a></cite>
-        <figcaption>Gallery</figcaption>
-    </figure>
-
-    <figure>
-    <!-- Source:https://d33wubrfki0l68.cloudfront.net/images/festimgs/b8e776b7f18e72feecc198ee869c4f2c278d7b4e/img_4461.jpg -->
-    <img src = "images/gallery8.jpg
-    " alt = "Gallery8"/>
-    Source: <cite><a href="https://d33wubrfki0l68.cloudfront.net/images/festimgs/b8e776b7f18e72feecc198ee869c4f2c278d7b4e/img_4461.jpg">Gallery</a></cite>
-    <figcaption>Gallery</figcaption>
-    </figure>
-
-    <figure>
-    <!-- Source:https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi9hmRZGRQFMvKeIRdhOE2hqAspl67SK9hvullt9_l99Pk41pYkg -->
-    <img src = "images/gallery9.jpeg
-    " alt = "Gallery9"/>
-    Source: <cite><a href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi9hmRZGRQFMvKeIRdhOE2hqAspl67SK9hvullt9_l99Pk41pYkg">Gallery</a></cite>
-    <figcaption>Gallery</figcaption>
-    </figure>
-    </div>
-    <div class = "images3">
-    <figure>
-    <!-- Source:https://www.downtownithaca.com/wp-content/uploads/AppleHarvest-Classic.jpg -->
-    <img src = "images/gallery10.jpg
-    " alt = "Gallery10"/>
-    Source: <cite><a href="https://www.downtownithaca.com/wp-content/uploads/AppleHarvest-Classic.jpg">Gallery</a></cite>
-    <figcaption>Gallery</figcaption>
-    </figure>
-
-    <figure>
-    <!-- Source:https://s3-media3.fl.yelpcdn.com/bphoto/ox63gqF_ob8V5LSsZ7HjpQ/ls.jpg -->
-    <img src = "images/gallery11.jpg
-    " alt = "Gallery11"/>
-    Source: <cite><a href="https://s3-media3.fl.yelpcdn.com/bphoto/ox63gqF_ob8V5LSsZ7HjpQ/ls.jpg">Gallery</a></cite>
-    <figcaption>Gallery</figcaption>
-    </figure>
-
-    <figure>
-    <!-- Source:https://s3.amazonaws.com/exposure-media/production/photos/dvykxxyvmvl472wy1i2ju4n29czznsxeszdx/original.jpg?fm=pjpg&auto=format&fm=jpg&w=300 -->
-    <img src = "images/gallery12.jpg
-    " alt = "Gallery12"/>
-    Source: <cite><a href="https://s3.amazonaws.com/exposure-media/production/photos/dvykxxyvmvl472wy1i2ju4n29czznsxeszdx/original.jpg?fm=pjpg&auto=format&fm=jpg&w=300">Gallery</a></cite>
-    <figcaption>Gallery</figcaption>
-    </figure>
-</div>
-<?php
-include("includes/footer.php");
-$title = "Footer";
-$footer_nav_class = "current_page";
-
-?>
+  <?php include("includes/footer.php"); ?>
 </body>
 
-</HTML>
+</html>
